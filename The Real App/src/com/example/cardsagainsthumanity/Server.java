a4936f7b0f43b48ec1ab9ff12c822db2de006e8b
poplav92 <mikepop77@gmail.com>
2013-02-23 18:21:47 -0500

    mike init classes

---
+++ The Real App/src/com/example/cardsagainsthumanity/Server.java
@@ -0,0 +1,21 @@
+package com.example.cardsagainsthumanity;
+
+public class Server 
+{
+	User user;
+	
+	public void getUser()
+	{
+		
+	}
+	
+	public void getGameRound()
+	{
+		
+	}
+	
+	public void addUsers()
+	{
+		
+	}
+}
