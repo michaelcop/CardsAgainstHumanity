a68d7a817972220c564a75724b132b788c933083
DragonmasterJ <DragonmasterJ@knights.ucf.edu>
2013-02-23 19:37:44 -0500

    Worked on login activity

---
+++ The Real App/src/com/example/cardsagainsthumanity/LogInActivity.java
@@ -0,0 +1,41 @@
+package com.example.cardsagainsthumanity;
+
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+public class LogInActivity extends Activity
+{
+	public void onCreate(Bundle savedInstance State)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		
+		inputUsername = (EditText) findViewById(R.id.editText1);
+		inputPassword = (EditText) findViewById(R.id.editText2);
+		
+		Button login = (Button) findViewById(R.id.button1);
+		
+		login.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View arg0)
+			{
+				Intent nextScreen = new Intent(getApplicationContext(), MainMenuActivity.class)
+				
+				nextScreen.putExtra("Username", inputUsername.getText().toString());
+				nextScreen.putExtra("Password", inputPassword.getText().toString());
+				
+				startActivity(nextScreen);
+			}
+		})
+	}
+	
+
+}
---
+++ The Real App/src/com/example/cardsagainsthumanity/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.cardsagainsthumanity;
+
+public class MainMenuActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
