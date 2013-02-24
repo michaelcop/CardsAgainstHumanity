759e90ebb976d2d9b8cf52b5dfe7375c85e3e3f1
DragonmasterJ <DragonmasterJ@knights.ucf.edu>
2013-02-23 20:32:59 -0500

    Made changes to Start Pge

--- The Real App/src/com/example/cardsagainsthumanity/LogInActivity.java
+++ The Real App/src/com/example/cardsagainsthumanity/LogInActivity.java
@@ -1,14 +1,11 @@
 package com.example.cardsagainsthumanity;
 
-import android.R;
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
-import android.view.View.OnClickListener;
 
 
 public class LogInActivity extends Activity
--- The Real App/src/com/example/cardsagainsthumanity/StartPage.java
+++ The Real App/src/com/example/cardsagainsthumanity/StartPage.java
@@ -4,20 +4,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
 import android.content.Intent;
 
 public class StartPage extends Activity {
+	
+	EditText inputUsername;
+	EditText inputPassword;
+	String userName;
+	String password;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		Button v = (Button) findViewById(R.id.button1);
+		
+		inputUsername = (EditText) findViewById(R.id.editText1);
+		inputPassword = (EditText) findViewById(R.id.editText2);
+		
 		v.setOnClickListener(new OnClickListener() {
+			
 	        public void onClick(View v) {
-	        	Intent myIntent = new Intent(v.getContext(), MainMenu.class);
-                startActivityForResult(myIntent, 0);
+	        	
+	        	password = "test";
+	        	if(inputPassword.getText().toString() == password)
+	        	{
+	        		
+	        	
+		        	Intent myIntent = new Intent(v.getContext(), MainMenu.class);
+	                startActivityForResult(myIntent, 0);
+	        	}
 	        	}
 			
 			
