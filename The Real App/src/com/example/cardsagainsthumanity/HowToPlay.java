3799a6f3177c761a279c992b4172166b2e136e37
Mercy Peou <MariusAfricanius@gmail.com>
2013-03-10 12:54:18 -0400

    Cleaned up some files and edited  MainMenu

---
+++ The Real App/src/com/example/cardsagainsthumanity/HowToPlay.java
@@ -0,0 +1,16 @@
+package com.example.cardsagainsthumanity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.cardsagainsthumanity.Entities.R;
+
+public class HowToPlay extends Activity
+{
+
+}
--- The Real App/src/com/example/cardsagainsthumanity/MainMenu.java
+++ The Real App/src/com/example/cardsagainsthumanity/MainMenu.java
@@ -1,6 +1,7 @@
 package com.example.cardsagainsthumanity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -17,6 +18,7 @@ public class MainMenu extends Activity
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.mainmenu);
 	
+		//Create listener----------------------------------------------
 		Button create = (Button) findViewById(R.id.createButton);
 		
 		create.setOnClickListener(new OnClickListener()
@@ -29,6 +31,31 @@ public class MainMenu extends Activity
 			}
 			
 		});
+		
+		//Create listener end----------------------------------------------
+		
+		
+		//How to Play listener---------------------------------------------
+		
+		Button how = (Button) findViewById(R.id.howButton);
+		
+		how.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v) 
+			{
+				Intent myIntent = new Intent(v.getContext(), HowToPlay.class);
+                startActivityForResult(myIntent, 0);
+				
+			}
+			
+		});
+		
+		//How to Play listener end---------------------------------------------
+		
+		//Button how = (Button) findViewById(R.id.howButton);
+		
 		
 	}
 }
