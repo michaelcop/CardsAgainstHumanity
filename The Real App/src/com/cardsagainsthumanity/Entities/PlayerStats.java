80649f8bf0c447e09f877ae59cb10abead272a88
David Nguyen <david_nguyen@knights.ucf.edu>
2013-03-10 17:21:07 -0400

    Stats was worked on

--- The Real App/src/com/cardsagainsthumanity/Entities/PlayerStats.java
+++ The Real App/src/com/cardsagainsthumanity/Entities/PlayerStats.java
@@ -1,5 +1,30 @@
 package com.cardsagainsthumanity.Entities;
 
-public class PlayerStats {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+public class PlayerStats extends Activity
+{
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.playerstats);
+
+		Button returns = (Button) findViewById(R.id.button1);
+		returns.setOnClickListener(new OnClickListener()
+		{
+	
+			@Override
+			public void onClick(View v) 
+			{
+				finish();
+			}
+			
+		});
+	} 
 }
