59131259ac8363cfcc9462fb2e47e14a38a898a9
David Douberley <Davidhockey22ster@gmail.com>
2013-03-10 13:08:52 -0400

    Working on test servlet

--- ServletTest/src/Testing/Test.java
+++ ServletTest/src/Testing/Test.java
@@ -1,28 +1,66 @@
 package Testing;
 import java.io.*;
-import java.util.List;
+import java.sql.*;
+import java.util.logging.Logger;
+
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import javax.sql.DataSource;
 
 @WebServlet(urlPatterns={"/Test"})
-public class Test extends HttpServlet {
+public class Test extends HttpServlet implements DataSource {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -504282938198562128L;
 	private String Tested =  "Not set";
-	private Factory UsersFact = new Factory();
-	
+	Connection connection = null;
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-			out.println("Hello ");
+
+		try {
+		    System.out.println("Loading driver...");
+		    Class.forName("com.mysql.jdbc.Driver");
+		    System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+		    throw new RuntimeException("Cannot find the driver in the classpath!", e);
+		}
 		
-	
+		Test ds = new Test();
+        try {
+			connection = ds.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		PrintWriter out = response.getWriter();
+		if(connection != null){
+			
+			out.println("Hello ");
+			
+			Statement stmt;
+			ResultSet rs;
+			try {
+				stmt = connection.createStatement();
+				rs = stmt.executeQuery("SELECT * FROM tblUsers");
+				rs.close();
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+
+		
+		
 	}
 
 
@@ -33,5 +71,80 @@ public class Test extends HttpServlet {
 
 	public void setTested(String tested) {
 		Tested = tested;
+	}
+
+
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public Connection getConnection() throws SQLException {
+        if (connection != null) {
+            System.out.println("Cant craete a Connection");
+    } else {
+            connection = DriverManager.getConnection(
+                            "jdbc:mysql://ourdbinstance.cbvrc3frdaal.us-east-1.rds.amazonaws.com:3306/dbAppData", "AWSCards", "Cards9876");
+    }
+    return connection;
+	}
+
+
+	@Override
+	public Connection getConnection(String username, String password)
+			throws SQLException {
+		// TODO Auto-generated method stub
+        if (connection != null) {
+                System.out.println("Cant craete a Connection");
+        } else {
+                connection = DriverManager.getConnection(
+                                "jdbc:mysql://ourdbinstance.cbvrc3frdaal.us-east-1.rds.amazonaws.com:3306/dbAppData", username, password);
+        }
+        return connection;
 	}
 }
