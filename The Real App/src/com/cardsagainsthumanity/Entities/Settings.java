b50fae909abc823cd6e273648f8c2c37ad58e269
poplav92 <mikepop77@gmail.com>
2013-03-16 12:22:25 -0400

    mike editing settings.java

--- The Real App/src/com/cardsagainsthumanity/Entities/Settings.java
+++ The Real App/src/com/cardsagainsthumanity/Entities/Settings.java
@@ -1,5 +1,15 @@
 package com.cardsagainsthumanity.Entities;
-public class Settings 
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+import android.app.Activity;
+import android.content.*;
+import java.io.*;
+
+public class Settings extends Activity
 {
 	private User user;
 	private Game games;
@@ -38,5 +48,48 @@ public class Settings
 	void editPassWord()
 	{
 		
+	}
+	
+	void writeSettings()
+	{
+		/*
+		 * Tutorial located at 
+		 * http://stackoverflow.com/questions/1239026/how-to-create-a-file-in-android
+		 */
+		try
+		{
+			//write testString to file
+			String testString = "test";
+			FileOutputStream fOut = openFileOutput("samplefile",
+                    Context.MODE_PRIVATE);
+			OutputStreamWriter osw = new OutputStreamWriter(fOut);
+			
+			osw.write(testString);
+			
+			osw.flush();
+			osw.close();
+			
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+	}
+	
+	void readSettings()
+	{
+		try
+		{
+			FileInputStream fIn = openFileInput("samplefile.txt");
+	        InputStreamReader isr = new InputStreamReader(fIn);
+	        
+	        char[] inputBuffer = new char[100];
+	        isr.read(inputBuffer);
+	        String readString = new String(inputBuffer);
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
 	}
 }
