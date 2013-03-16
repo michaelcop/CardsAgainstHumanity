8e0fbcf632a87119f0e86f6469df03d093f35e28
David Nguyen <david_nguyen@knights.ucf.edu>
2013-03-16 14:12:56 -0400

    Added menu stuff

---
+++ The Real App/src/com/cardsagainsthumanity/Entities/AndroidMenusActivity.java
@@ -0,0 +1,67 @@
+package com.cardsagainsthumanity.Entities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class AndroidMenusActivity extends Activity{
+
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.mainmenu);
+	    }
+	 
+	    // Initiating Menu XML file (menu.xml)
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu)
+	    {
+	        MenuInflater menuInflater = getMenuInflater();
+	        menuInflater.inflate(R.menu.menu, menu);
+	        return true;
+	    }
+	 
+	    /**
+	     * Event Handling for Individual menu item selected
+	     * Identify single menu item by it's id
+	     * */
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item)
+	    {
+	 
+	        switch (item.getItemId())
+	        {
+	        case R.id.menu_bookmark:
+	            // Single menu item is selected do something
+	            // Ex: launching new activity/screen or show alert message
+	            Toast.makeText(AndroidMenusActivity.this, "Bookmark is Selected", Toast.LENGTH_SHORT).show();
+	            return true;
+	 
+	        case R.id.menu_save:
+	            Toast.makeText(AndroidMenusActivity.this, "Save is Selected", Toast.LENGTH_SHORT).show();
+	            return true;
+	 
+	        case R.id.menu_search:
+	            Toast.makeText(AndroidMenusActivity.this, "Search is Selected", Toast.LENGTH_SHORT).show();
+	            return true;
+	 
+	        case R.id.menu_share:
+	            Toast.makeText(AndroidMenusActivity.this, "Share is Selected", Toast.LENGTH_SHORT).show();
+	            return true;
+	 
+	        case R.id.menu_delete:
+	            Toast.makeText(AndroidMenusActivity.this, "Delete is Selected", Toast.LENGTH_SHORT).show();
+	            return true;
+	 
+	        case R.id.menu_preferences:
+	            Toast.makeText(AndroidMenusActivity.this, "Preferences is Selected", Toast.LENGTH_SHORT).show();
+	            return true;
+	 
+	        default:
+	            return super.onOptionsItemSelected(item);
+	        }
+	    }    
+	 
+	}
