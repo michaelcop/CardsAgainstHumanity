c5df7ff57357c7ab4681abd3406310c959202ea0
David Nguyen <david_nguyen@knights.ucf.edu>
2013-03-28 21:59:43 -0400

    Created an activity to invite friends.

---
+++ The Real App/src/com/cardsagainsthumanity/Entities/InviteFriends.java
@@ -0,0 +1,33 @@
+package com.cardsagainsthumanity.Entities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InviteFriends extends Activity
+{
+
+}
