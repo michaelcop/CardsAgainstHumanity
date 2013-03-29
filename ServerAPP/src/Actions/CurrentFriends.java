56ceb3fa8ccd705071ca464eed5a619ae7213891
Jimmy Eckstein <jimmyeckstein@gmail.com>
2013-03-29 18:23:30 -0400

    View current friends (accepted friends)

---
+++ ServerAPP/src/Actions/CurrentFriends.java
@@ -0,0 +1,170 @@
+package src.Actions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet(urlPatterns={"/CurrentFriends"})
+public class CurrentFriends extends HttpServlet implements DataSource{
+	private String User = "";
+	Connection connection = null;
+	//int UserID = 0;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		if(request.getParameter("User") != null){ 
+			this.setUser((String) request.getParameter("User").toString());
+			//UserID = Integer.parseInt(User);
+		}
+		try {
+		    System.out.println("Loading driver...");
+		    Class.forName("com.mysql.jdbc.Driver");
+		    System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+		    throw new RuntimeException("Cannot find the driver in the classpath!", e);
+		}
+		
+		CurrentFriends ds = new CurrentFriends();
+        try {
+			connection = ds.getConnection();
+			System.out.println("connection made");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		PrintWriter out = response.getWriter();
+		//Check connection has been made
+		if(connection != null){
+			Statement stmt;
+			ResultSet rs;
+			try {				
+				stmt = connection.createStatement();
+				
+				//Query to view Accepted friends
+				rs = stmt.executeQuery("SELECT tblUsers_1.Username, tblUsers_1.UserID FROM tblUsers INNER JOIN (tblUsers AS tblUsers_1 INNER JOIN tblFriends ON tblUsers_1.UserID = tblFriends.FriendUserID) ON tblUsers.UserID = tblFriends.UserID WHERE tblUsers.Username = '"+ User +"' AND tblFriends.FriendStatus = 1;");
+				
+				//If user has no friends
+				if(!rs.isBeforeFirst()){
+					out.println("Friends;None;-1");
+				}
+				else{
+					//Display users friends
+					out.print("Friends");
+					while(rs.next()){
+						//System.out.println("in while");
+						//System.out.println("Friend Username: " + rs.getString(1) + " Friend Status: " + rs.getInt(2) );
+						out.print(";" + rs.getString(1) + ";" + rs.getInt(2));
+					}
+				}
+			
+				//Close connection
+				rs.close();
+				stmt.close();
+				connection.close();
+			} 
+			catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public Connection getConnection() throws SQLException {
+        if (connection != null) {
+            System.out.println("Cant craete a Connection");
+    } else {
+            connection = DriverManager.getConnection(
+                            "jdbc:mysql://ourdbinstance.cbvrc3frdaal.us-east-1.rds.amazonaws.com:3306/dbAppData", "AWSCards", "Cards9876");
+    }
+    return connection;
+	}
+
+
+	@Override
+	public Connection getConnection(String username, String password)
+			throws SQLException {
+		// TODO Auto-generated method stub
+        if (connection != null) {
+                System.out.println("Cant create a Connection");
+        } else {
+                connection = DriverManager.getConnection(
+                                "jdbc:mysql://ourdbinstance.cbvrc3frdaal.us-east-1.rds.amazonaws.com:3306/dbAppData", username, password);
+        }
+        return connection;
+	}
+
+	public String getUser() {
+		return User;
+	}
+
+
+	public void setUser(String user) {
+		User = user;
+	}
+	
+}
