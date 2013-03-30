7e9b47aeb428c2a2c4f2290fb1b3ba8f14471cb1
Mercy Peou <mpeou@knights.ucf.edu>
2013-03-30 15:00:18 -0400

    Working on JoinGame

--- The Real App/src/com/cardsagainsthumanity/Entities/JoinGame.java
+++ The Real App/src/com/cardsagainsthumanity/Entities/JoinGame.java
@@ -1,20 +1,36 @@
 package com.cardsagainsthumanity.Entities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class JoinGame extends Activity
 {
+	Context context = this;
+	
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.joingame);
-
+		Bundle extras = getIntent().getExtras();
+		
+		Toast.makeText(context, "CallingURL", Toast.LENGTH_LONG).show();
+		
+		Button join = (Button) findViewById(R.id.joinButton);
+		join.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				
+			}
+		});
+		
 		Button returns = (Button) findViewById(R.id.ReturnToMenu);
 		returns.setOnClickListener(new OnClickListener()
 		{
