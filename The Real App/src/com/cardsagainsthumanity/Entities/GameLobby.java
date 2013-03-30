d4a88c0ef4635be211a984dc155a41dc1753d843
poplav92 <mikepop77@gmail.com>
2013-03-30 12:36:26 -0400

    mike update game lobby

--- The Real App/src/com/cardsagainsthumanity/Entities/GameLobby.java
+++ The Real App/src/com/cardsagainsthumanity/Entities/GameLobby.java
@@ -62,7 +62,7 @@ public class GameLobby extends Activity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
+        menuInflater.inflate(R.menu.invite, menu);
         Toast.makeText(GameLobby.this, "In menue at create game", Toast.LENGTH_SHORT).show();
         return true;
     }
@@ -87,7 +87,7 @@ public class GameLobby extends Activity
 	public void refreshGameLobby()
 	{
 		//URL contains the userID and gameID
-		String stringUrl = "http://54.225.225.185:8080/ServerAPP/UserGameLobby?User=" + userID + "&gameID="+gameID;
+		String stringUrl = "http://54.225.225.185:8080/ServerAPP/UserGameLobby?gameID="+gameID;
     	check = "GameLobby";
     	ConnectivityManager connMgr = (ConnectivityManager) 
 		getSystemService(Context.CONNECTIVITY_SERVICE);
