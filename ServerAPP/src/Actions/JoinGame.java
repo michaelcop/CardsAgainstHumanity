d02f2f0740a07e58f5bff99ca26ea908e8235957
Jimmy Eckstein <jimmyeckstein@gmail.com>
2013-04-12 19:50:39 -0400

    Join game now gets a hand for a user

--- ServerAPP/src/Actions/JoinGame.java
+++ ServerAPP/src/Actions/JoinGame.java
@@ -1,6 +1,9 @@
-package Actions;
+package src.Actions;
 import java.io.*;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.*;
@@ -23,6 +26,10 @@ public class JoinGame extends HttpServlet implements DataSource {
 	//GameID that user is being invited to
 	private String Game = null;
 	private int GameID = 0;
+	
+	//private String PlayerHand = null;
+	private String userCards = null;
+	private String Cards = null;
 	
 	Connection connection = null;
 
@@ -58,13 +65,53 @@ public class JoinGame extends HttpServlet implements DataSource {
 		//Check connection has been made
 		if(connection != null){
 			Statement stmt;
-			//ResultSet rs;
-			int rs;
+			ResultSet rs2;
+			int rs, rs3;
 			try {				
 				stmt = connection.createStatement();
-		
+				
+				//Query deck that is being used in for game
+				rs2 = stmt.executeQuery("SELECT GameDeck FROM tblGames WHERE GameID = "+ GameID +";");
+				
+				if(rs2.next())
+				{
+				   //pull five cards off the top of the deck
+				   Cards = rs2.getString(1);
+				   //out.println("Old Cards: " + Cards);
+	               String[] cards;
+	               cards = Cards.split(";");
+	               List<String> cardsList = new ArrayList(Arrays.asList(cards));
+	               List<String> userCardsList = new ArrayList();
+	               for(int i=0; i<5; i++){
+	                       userCardsList.add(cardsList.get(0));
+	                       cardsList.remove(0);
+	               }
+	               Cards = null;
+	               for(String x: cardsList){
+	                       if(Cards==null){
+	                               Cards = x;
+	                       }
+	                       else{
+	                               Cards = Cards+ ";" + x;
+	                       }
+	               }
+	               userCards = null;
+	               for(String x: userCardsList){
+	                       if(userCards==null){
+	                               userCards = x;
+	                       }
+	                       else{
+	                               userCards = userCards+ ";" + x;
+	                       }
+	               }
+		               
+		           //out.println("New Cards : " + Cards);
+		           //out.println("User Hand: " + userCards);
+				}
+				
+				
 				//Query to Update the joining user's PlayerStatus to 'In Game' - 1
-				rs = stmt.executeUpdate("UPDATE tblPlayers SET PlayerStatus = 1 WHERE PlayerGameID = "+ GameID +" AND PlayerUserID = "+ UserID +";");
+				rs = stmt.executeUpdate("UPDATE tblPlayers SET PlayerStatus = 1, PlayerHand = '"+ userCards +"' WHERE PlayerGameID = "+ GameID +" AND PlayerUserID = "+ UserID +";");
 				
 				//Check if update was successful
 				if(rs != 0){
@@ -76,7 +123,21 @@ public class JoinGame extends HttpServlet implements DataSource {
 					out.println("Error: Unable to join game");
 				}
 				
+				//Place deck back into game table
+				rs3 = stmt.executeUpdate("UPDATE tblGames SET GameDeck = '"+ Cards +"' WHERE GameID = "+ GameID +";");
+				
+				//Check if update was successful
+				if(rs3 != 0){
+					//Cards updated in games table
+					//out.println("Joining");
+				}
+				else{
+					//Unable to join game
+					out.println("Error: Unable to update cards in games table");
+				}
+				
 				//Close record sets and connection
+				rs2.close();
 				stmt.close();
 				connection.close();	
 			}
